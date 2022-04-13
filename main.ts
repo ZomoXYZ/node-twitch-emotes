f30c652dc738938137bf92fad56aafb60fa0f3b1
ashley zomo <ashleyzomo@gmail.com>
2022-04-13 15:05:02 -0500

    removed todos

--- main.ts
+++ main.ts
@@ -2,10 +2,6 @@ import { highestQuality, initCache, spliceMessage } from './ttvemotes'
 
 initCache()
 
-//todo init with channel names (will only redownload if timestamp needs it)
-//todo auto reload occasionally (should it? or is this a program that will be restarted regularly)
-//todo add reloadAll()
-
 spliceMessage('OMEGALUL Clap', 'xqcow', emote => {
 	console.log(emote)
 	return highestQuality(emote)

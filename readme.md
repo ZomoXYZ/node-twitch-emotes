63b031b93de2dc99ee573d3a87bc5c0d968c3551
ZomoXYZ <ashleyzomo@gmail.com>
2022-06-11 22:09:30 -0500

    asshole changed his username and broke my code

--- readme.md
+++ readme.md
@@ -11,9 +11,9 @@ simple
 ```ts
 import { initCache, spliceMessage } from 'twitch-emotes'
 
-await initCache(['xqcow'])
+await initCache(['xqc'])
 
-let message = spliceMessage('EZ Clap too good', 'xqcow')
+let message = spliceMessage('EZ Clap too good', 'xqc')
 console.log(message)
 
 // [ 'https://cdn.betterttv.net/emote/5590b223b344e2c42a9e28e3/3x', 'https://cdn.betterttv.net/emote/55b6f480e66682f576dd94f5/3x', 'too', 'good' ]
@@ -24,9 +24,9 @@ advanced
 ```ts
 import { initCache, spliceMessage } from 'twitch-emotes'
 
-await initCache(['xqcow'])
+await initCache(['xqc'])
 
-let message = spliceMessage('EZ Clap too good', 'xqcow', emote => {
+let message = spliceMessage('EZ Clap too good', 'xqc', emote => {
     return `emote:${emote.code}`
 })
 console.log(message)
--- tests/spliceMessage.js
+++ tests/spliceMessage.js
@@ -3,9 +3,9 @@ import { compareArray } from './util/index.js'
 
 export default async function () {
     //set autoRefresh to false so the script ends naturally
-    await initCache(['xqcow'], { autoRefresh: false, cache: false, logApiRate: false })
+    await initCache(['xqc'], { autoRefresh: false, cache: false, logApiRate: false })
 
-    const withLinks = spliceMessage('EZ Clap too good', 'xqcow')
+    const withLinks = spliceMessage('EZ Clap too good', 'xqc')
     const withLinksExpected = [
         'https://cdn.betterttv.net/emote/5590b223b344e2c42a9e28e3/3x',
         'https://cdn.betterttv.net/emote/55b6f480e66682f576dd94f5/3x',
@@ -14,7 +14,7 @@ export default async function () {
     ]
     compareArray(withLinks, withLinksExpected)
 
-    const asStrings = spliceMessage('EZ Clap too good', 'xqcow', emote => `emote:${emote.code}`)
+    const asStrings = spliceMessage('EZ Clap too good', 'xqc', emote => `emote:${emote.code}`)
     const asStringsExpected = ['emote:EZ', 'emote:Clap', 'too', 'good']
     compareArray(asStrings, asStringsExpected)
 }

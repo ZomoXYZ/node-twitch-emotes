f0589b91fee2a7b909856fd6553869bd2e691a1d
zomo <ashleyzomo@gmail.com>
2023-08-23 01:27:03 -0500

    change: allow use of irc tags for emotes

--- src/index.ts
+++ src/index.ts
@@ -10,27 +10,68 @@ export * from './util'
 /**
  *
  * @param callback default callback is `highestQuality`
+ * @param withEmtoes the `emotes` tag from the twitch message
  * @returns
  */
 export function spliceMessage<T>(
     message: string,
     channel: string,
-    callback: (emote: EmoteData) => string | T = highestQuality
+    callback: (emote: EmoteData) => string | T = highestQuality,
+    withEmtoes?: string
 ): (string | T)[] {
     isChannelThrow(channel)
 
-    const messageSpl: string[] = message.split(' '),
+    var messageSpl: (string | EmoteData)[] = message.split(' '),
         arr: (string | T)[] = []
+
+    if (withEmtoes) {
+        messageSpl = []
+        var lastChar = 0
+
+        withEmtoes.split('/').forEach(spl => {
+            const [id, range] = spl.split(':')
+            const [start, end] = range.split('-').map(Number)
+            const emote: EmoteData = {
+                provider: 0,
+                code: message.slice(start, end + 1),
+                urls: [
+                    {
+                        size: '1x',
+                        url: `https://static-cdn.jtvnw.net/emoticons/v2/${id}/default/dark/1.0`,
+                    },
+                    {
+                        size: '2x',
+                        url: `https://static-cdn.jtvnw.net/emoticons/v2/${id}/default/dark/2.0`,
+                    },
+                    {
+                        size: '3x',
+                        url: `https://static-cdn.jtvnw.net/emoticons/v2/${id}/default/dark/3.0`,
+                    },
+                ],
+            }
+
+            messageSpl.push(...message.slice(lastChar, start).split(' '))
+            if (messageSpl.at(-1) === '') messageSpl.pop()
+
+            messageSpl.push(emote)
+
+            lastChar = end + 1
+            if (message[end + 1] === ' ') lastChar++
+        })
+
+        messageSpl.push(...message.slice(lastChar).split(' '))
+        if (messageSpl.at(-1) === '') messageSpl.pop()
+    }
 
     for (let i = 0; i < messageSpl.length; i++) {
         const word = messageSpl[i]
 
-        let emote = getEmote(word, channel)
+        let emote = typeof word === 'string' ? getEmote(word, channel) : word
 
         if (emote) {
             arr[i] = callback(emote)
         } else {
-            arr[i] = word
+            arr[i] = word as string // this is logically a string
         }
     }
 
--- tests/spliceMessage.js
+++ tests/spliceMessage.js
@@ -8,7 +8,13 @@ export default async function () {
     await initCache(['xqc'], { autoRefresh: false, cache: false, logApiRate: false })
 
     await runTest(testAsURL, 'As Url')
+    await runTest(testAsURLWithEmotes, 'As Url With Emotes')
+    await runTest(testAsURLWithEmotesCombined, 'As Url With Emotes Combined')
+
     await runTest(testAsString, 'As String')
+    await runTest(testAsStringWithEmotes, 'As String With Emotes')
+    await runTest(testAsStringWithEmotesCombined, 'As String With Emotes Combined')
+
     await runTest(testCacheError, 'Cache Error')
     await runTest(testUsernameInvalid, 'Username Invalid')
     await runTest(testUsernameError, 'Username Error')
@@ -26,9 +32,72 @@ function testAsURL() {
     compareArray(message, expected)
 }
 
+function testAsURLWithEmotes() {
+    const message = spliceMessage(
+        'hasHi hasSlam hasPOGGERS hasPOGGIES',
+        'xqc',
+        undefined,
+        'emotesv2_8b228c4bd87b4305a4c05179353e751a:0-4/emotesv2_4f058d58458544a4971de55672468204:6-12/302587322:14-23/303446318:25-34'
+    )
+    const expected = [
+        'https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_8b228c4bd87b4305a4c05179353e751a/default/dark/3.0',
+        'https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_4f058d58458544a4971de55672468204/default/dark/3.0',
+        'https://static-cdn.jtvnw.net/emoticons/v2/302587322/default/dark/3.0',
+        'https://static-cdn.jtvnw.net/emoticons/v2/303446318/default/dark/3.0',
+    ]
+    compareArray(message, expected)
+}
+
+function testAsURLWithEmotesCombined() {
+    const message = spliceMessage(
+        'EZ Clap hasPOGGERS hasPOGGIES too good',
+        'xqc',
+        undefined,
+        '302587322:8-17/303446318:19-28'
+    )
+    const expected = [
+        'https://cdn.7tv.app/emote/63071b80942ffb69e13d700f/4x.webp',
+        'https://cdn.7tv.app/emote/62fc0a0c4a75fd54bd3520a9/4x.webp',
+        'https://static-cdn.jtvnw.net/emoticons/v2/302587322/default/dark/3.0',
+        'https://static-cdn.jtvnw.net/emoticons/v2/303446318/default/dark/3.0',
+        'too',
+        'good',
+    ]
+    compareArray(message, expected)
+}
+
 function testAsString() {
     const message = spliceMessage('EZ Clap too good', 'xqc', emote => `emote:${emote.code}`)
     const expected = ['emote:EZ', 'emote:Clap', 'too', 'good']
+    compareArray(message, expected)
+}
+
+function testAsStringWithEmotes() {
+    const message = spliceMessage(
+        'hasHi hasSlam hasPOGGERS hasPOGGIES',
+        'xqc',
+        emote => `emote:${emote.code}`,
+        'emotesv2_8b228c4bd87b4305a4c05179353e751a:0-4/emotesv2_4f058d58458544a4971de55672468204:6-12/302587322:14-23/303446318:25-34'
+    )
+    const expected = ['emote:hasHi', 'emote:hasSlam', 'emote:hasPOGGERS', 'emote:hasPOGGIES']
+    compareArray(message, expected)
+}
+
+function testAsStringWithEmotesCombined() {
+    const message = spliceMessage(
+        'EZ Clap hasPOGGERS hasPOGGIES too good',
+        'xqc',
+        emote => `emote:${emote.code}`,
+        '302587322:8-17/303446318:19-28'
+    )
+    const expected = [
+        'emote:EZ',
+        'emote:Clap',
+        'emote:hasPOGGERS',
+        'emote:hasPOGGIES',
+        'too',
+        'good',
+    ]
     compareArray(message, expected)
 }
 
